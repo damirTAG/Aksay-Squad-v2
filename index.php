393fcae2a95009666e86da98280faccd9ad143c2
damirTAG <93281431+damirTAG@users.noreply.github.com>
2023-04-18 00:21:40 +0600

    Update index.php

--- index.php
+++ index.php
@@ -62,45 +62,6 @@ if (isset($_GET['id'])) {
         </header>
 <!-- велком -->
     <section class="welcome">
-            
-            <!-- <div class="bg-vector-right">
-            <svg width="245" height="418" viewBox="0 0 245 418" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g filter="url(#filter0_d_28_10)">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M426.485 182.756C426.949 241.702 366.568 279.076 320.566 316.044C286.939 343.066 246.192 350.05 204.488 361.157C150.164 375.625 86.4386 427.138 44.7437 389.503C2.49461 351.369 45.6869 281.866 54.362 225.652C59.6978 191.076 71.4912 161.729 84.8836 129.398C108.031 73.5178 102.629 -7.807 159.586 -28.3698C216.647 -48.9697 270.887 7.47374 318.431 45.0826C366.013 82.7214 426.008 122.139 426.485 182.756Z" fill="#FFE601"/>
-            </g>
-            <defs>
-            <filter id="filter0_d_28_10" x="0.500977" y="-57.7644" width="439.987" height="475.762" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-            <feOffset dx="-6" dy="-5"/>
-            <feGaussianBlur stdDeviation="10"/>
-            <feComposite in2="hardAlpha" operator="out"/>
-            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_28_10"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_28_10" result="shape"/>
-            </filter>
-            </defs>
-            </svg>
-            </div>
-            <div class="bg-vector-left">
-            <svg width="452" height="534" viewBox="0 0 452 534" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g filter="url(#filter0_d_27_6)">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M102.611 18.8461C144.247 37.4109 162.488 83.3888 195.488 114.579C223.901 141.435 255.875 161.074 282.921 189.27C327.799 236.055 414.388 270.5 406.261 334.309C398.482 395.389 306.05 401.283 252.007 431.99C211.963 454.742 176.395 490.257 130.147 491.193C86.0551 492.086 53.9498 452.286 12.6854 436.835C-29.0743 421.197 -78.7568 428.171 -113.184 400.075C-151.479 368.824 -186.594 323.936 -185.644 274.981C-184.699 226.268 -129.339 197.976 -108.408 153.89C-85.0003 104.587 -102.503 31.9794 -56.2063 2.35115C-10.6882 -26.7786 53.0715 -3.24321 102.611 18.8461Z" fill="#FFE601"/>
-                </g>
-                <defs>
-                <filter id="filter0_d_27_6" x="-185.663" y="-11.2277" width="637.458" height="544.435" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                <feOffset dx="25" dy="22"/>
-                <feGaussianBlur stdDeviation="10"/>
-                <feComposite in2="hardAlpha" operator="out"/>
-                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_27_6"/>
-                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_27_6" result="shape"/>
-                </filter>
-                </defs>
-                </svg>
-            </div> -->
     <div class="container">
         <div class="welcome-inner">
             <div class="w-text">
