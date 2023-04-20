7a9658277f8b581f2a42be472b21e75d3ccc15df
damirTAG <93281431+damirTAG@users.noreply.github.com>
2023-04-21 05:42:11 +0600

    Delete dbcon.php

--- dbcon.php
+++
@@ -1,7 +0,0 @@
-<!-- //dbcon.php -->
-<?php
-$conn = new mysqli('localhost','root','','notes');
-if ($conn->connect_error) {
-    die('Error : ('. $conn->connect_errno .') '. $conn->connect_error);
-}
-?>
