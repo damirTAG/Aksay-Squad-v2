7d9e8b793169214f70749d421484191590ec8e32
Damir Tagilbayev <93281431+damirTAG@users.noreply.github.com>
2023-11-01 18:16:58 +0600

    User action updated
    See changelog

---
+++ Aksay Squad/create.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set("Asia/Almaty");
+/** @var Connection $connection */
+$connection = require_once 'pdo.php';
+
+// Validate note object;
+
+$id = $_POST['id'] ?? '';
+if ($id){
+    $connection->updateNote($id, $_POST);
+} else {
+    $connection->addNote($_POST);
+}
+
+header('Location: index.php');
